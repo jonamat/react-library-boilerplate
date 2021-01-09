f4ee5dcce046c1baec2b9feec38b333ec9336edf
Jonathan Mataloni <jo.mataloni@gmail.com>
2021-01-09 10:52:37 +0100

    Update test configuration

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,9 @@
-// For a detailed explanation regarding each configuration property, visit:
-// https://jestjs.io/docs/en/configuration.html
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
 
 module.exports = {
-    globals: {
-        'ts-jest': {
-            diagnostics: false
-        },
-    },
     // All imported modules in your tests should be mocked automatically
     // automock: false,
 
@@ -63,25 +60,30 @@ module.exports = {
     // globalTeardown: undefined,
 
     // A set of global variables that need to be available in all test environments
-    // globals: {},
+    globals: {
+        'ts-jest': {
+            diagnostics: false,
+            isolatedModules: true,
+        },
+    },
 
     // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
     // maxWorkers: "50%",
 
     // An array of directory names to be searched recursively up from the requiring module's location
-    //   moduleDirectories: [
-    //     "node_modules"
-    //   ],
+    // moduleDirectories: [
+    //   "node_modules"
+    // ],
 
     // An array of file extensions your modules use
-    moduleFileExtensions: [
-        "js",
-        "json",
-        "jsx",
-        "ts",
-        "tsx",
-        "node"
-    ],
+    // moduleFileExtensions: [
+    //   "js",
+    //   "json",
+    //   "jsx",
+    //   "ts",
+    //   "tsx",
+    //   "node"
+    // ],
 
     // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
     // moduleNameMapper: {},
@@ -120,9 +122,7 @@ module.exports = {
     // rootDir: undefined,
 
     // A list of paths to directories that Jest should use to search for files in
-    // roots: [
-    //     "<rootDir>/src"
-    // ],
+    roots: ['<rootDir>/src', '<rootDir>/tests'],
 
     // Allows you to use a custom runner instead of Jest's default test runner
     // runner: "jest-runner",
@@ -133,11 +133,14 @@ module.exports = {
     // A list of paths to modules that run some code to configure or set up the testing framework before each test
     // setupFilesAfterEnv: [],
 
+    // The number of seconds after which a test is considered as slow and reported as such in the results.
+    // slowTestThreshold: 5,
+
     // A list of paths to snapshot serializer modules Jest should use for snapshot testing
     // snapshotSerializers: [],
 
     // The test environment that will be used for testing
-    testEnvironment: 'jsdom',
+    // testEnvironment: "jest-environment-jsdom",
 
     // Options that will be passed to the testEnvironment
     // testEnvironmentOptions: {},
@@ -172,13 +175,12 @@ module.exports = {
     // timers: "real",
 
     // A map from regular expressions to paths to transformers
-    // transform: {
-    //     "^.+\\.tsx?$": "ts-jest"
-    // },
+    // transform: undefined,
 
     // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
     // transformIgnorePatterns: [
-    //   "\\\\node_modules\\\\"
+    //   "\\\\node_modules\\\\",
+    //   "\\.pnp\\.[^\\\\]+$"
     // ],
 
     // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
