cdf07b3599320b9f0510d6bc2294ae8270e9e74d
Jonathan Mataloni <jo.mataloni@gmail.com>
2021-01-10 12:55:38 +0100

    Update build & tests stats

--- lib/esm/MyComponent/index.d.ts
+++ lib/esm/MyComponent/index.d.ts
@@ -1,8 +1,12 @@
 import { FC, ReactNode } from 'react';
-interface MyComponentProps {
+export interface MyComponentProps {
     /** Inline prop doc example. Default `world` */
     myProp?: ReactNode;
 }
-/** MyComponent description. See [Docs](https://github.com/jonamat/react-library-boilerplate). */
+/**
+ * MyComponent description.
+ *
+ * See [Docs](https://github.com/jonamat/react-library-boilerplate).
+ */
 declare const MyComponent: FC<MyComponentProps>;
 export default MyComponent;
